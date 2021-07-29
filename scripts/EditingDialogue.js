262d32044ec66435941dd97fe4ae42c9bcb553ab
Tobias Franz <cinex.mail@web.de>
2021-07-29 10:35:08 +0200

    The translation data is now loaded when you change the selected module and is displayed in the table correctly.
    Signed-off-by: Tobias Franz <tobias.franz@mydata.de>

--- scripts/EditingDialogue.js
+++ scripts/EditingDialogue.js
@@ -1,4 +1,4 @@
-import {MODULE_ID, TRANSLATION} from "./settings.js";
+import {MODULE_ID, TRANSLATION} from "./settings.js"
 
 export class EditingDialogue extends FormApplication {
 
@@ -9,7 +9,7 @@ export class EditingDialogue extends FormApplication {
     }
 
     async _updateObject(event, formData) {
-        return Promise.resolve(undefined);
+        return Promise.resolve(undefined)
     }
 
     static get defaultOptions() {
@@ -32,35 +32,57 @@ export class EditingDialogue extends FormApplication {
     }
 
     /**
-     * @param {Object} module
-     * @returns {{string: {translations: {language: string, text: string}}}}
+     * Async for each loop
+     *
+     * @param  {array} array - Array to loop through
+     * @param  {function} callback - Function to apply to each array item loop
      */
-    static loadTranslationsForModule(module) {
+    static async asyncForEach(array, callback) {
+        for (let index = 0; index < array.length; index += 1) {
+            await callback(array[index], index, array)
+        }
+    }
+
+    /**
+     * @param {Object} module
+     * @returns {[{string: {translations: {language: string, text: string}}}, int]}
+     */
+    static async loadTranslationsForModule(module) {
         console.log(`Loading translations for ${module.data.name}.`)
         const languages = module.languages
         let out = {}
         if (languages.length > 0) {
-            languages.forEach(((language) => {
+            await this.asyncForEach(languages, async (language) => {
                 console.log(`Current language key: ${language.lang}`)
-                if (typeof out["testKey"] === "undefined") {
-                    out["testKey"] = {
-                        translations: {}
+                let request = await fetch(language.path)
+                let languageData = await request.json()
+                console.log(languageData)
+                for (let languageDataKey in languageData) {
+                    if (!languageData.hasOwnProperty(languageDataKey)) {
+                        continue
                     }
+                    if (typeof out[languageDataKey] === "undefined") {
+                        out[languageDataKey] = {
+                            translations: {}
+                        }
+                    }
+                    out[languageDataKey].translations[language.lang] = languageData[languageDataKey]
                 }
-                out["testKey"].translations[language.lang] = language.name
-            }))
+            })
         }
 
-        return out
+        return [out, languages]
     }
 
     static async loadTranslations() {
-        game.modules.forEach(((module) => {
+        game.modules.forEach((async (module) => {
             if (module.active) {
-                let translationsForModule = EditingDialogue.loadTranslationsForModule(module)
+                // noinspection ES6RedundantAwait
+                let [translationsForModule, languages] = await EditingDialogue.loadTranslationsForModule(module)
                 if (Object.keys(translationsForModule).length > 0) {
                     this.TRANSLATIONS[module.id] = {
-                        name        : module.data.title,
+                        name:         module.data.title,
+                        languages:    languages,
                         translations: translationsForModule
                     }
                 }
@@ -70,7 +92,48 @@ export class EditingDialogue extends FormApplication {
 
     static async displayTranslationsForModule(moduleId) {
         const data = this.TRANSLATIONS[moduleId]
-        alert(`<pre>${JSON.stringify(data)}</pre>`)
+        if (!data) {
+            console.error(`Tried to fetch translations for unknown moduleId ${moduleId}.`)
+            return
+        }
+        const languages = data.languages
+        console.log(`Translation data for module ${moduleId}.`)
+        let tableHead = '<tr>'
+        tableHead += '<th>Key</th>'
+        for (let language in languages) {
+            if (!languages.hasOwnProperty(language)) {
+                continue
+            }
+            tableHead += `<th>${languages[language].name}</th>`
+        }
+        tableHead += '</tr>'
+        console.log(`Table Head Content: ${tableHead}`)
+
+        let tableBody = ''
+        let translationData = data.translations
+        for (let translationKey in translationData) {
+            if (!translationData.hasOwnProperty(translationKey)) {
+                continue
+            }
+            let translations = translationData[translationKey].translations
+            tableBody += '<tr>'
+            tableBody += `<td>${translationKey}</td>`
+            for (let i = 0; i < languages.length; i++) {
+                let currentLanguageIdentifier = languages[i].lang
+                if (translations.hasOwnProperty(currentLanguageIdentifier)) {
+                    tableBody += `<td>${translations[currentLanguageIdentifier]}</td>`
+                } else {
+                    tableBody += '<td></td>'
+                }
+            }
+            tableBody += '</tr>'
+        }
+        console.log(`Table Body Content: ${tableBody}`)
+
+        let table = $('#te-form table')
+        table.find('> thead').html(tableHead)
+        table.find('> tbody').html(tableBody)
+        console.log('Finished updating table.')
     }
 
     toggleSelect() {
@@ -80,14 +143,15 @@ export class EditingDialogue extends FormApplication {
         if (select.hasClass("disabled")) {
             select.removeClass('disabled').attr('disabled', false)
             loadingIcon.addClass('hidden')
-        } else {
+        }
+        else {
             select.addClass('disabled').attr('disabled', true)
             loadingIcon.removeClass('hidden')
         }
     }
 
     activateListeners(html) {
-        super.activateListeners(html);
+        super.activateListeners(html)
 
         $('#translation-editor-editor').find('select.moduleList').on('change', async function () {
             await EditingDialogue.displayTranslationsForModule($(this).val())
